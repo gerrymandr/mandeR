7deb97a1c51598e98e91e12419f21fe0c286a625
Richard Barnes <rbarnes@umn.edu>
2017-08-10 15:10:56 -0400

    Updated documentation

--- R/R/compactness.R
+++ R/R/compactness.R
@@ -10,8 +10,6 @@
 #' @import rgeos
 
 
-#' @name dg_exe_path
-#' 
 #' @title Get path to dggrid executable
 #'
 #' @description
@@ -24,14 +22,6 @@ mass_cd <- function(){
 }
 
 
-
-dgtransform <- function(dggs, lat, lon){ #TODO: Make sure we're not modifying the original dggs
-  dgverify(dggs)
-
-  warning("The 'dgtransform' function has been deprecated. Please use 'dgGEO_to_SEQNUM' instead!")
-
-  dgGEO_to_SEQNUM(dggs, lon, lat)$seqnum
-}
 
 allmetricscalc <- function(shape){#Calculates all metrics and adds output metrics to the spatial dataframe attributes
   spdf<-sp::fortify(shape)
@@ -59,16 +49,16 @@ allmetricscalc <- function(shape){#Calculates all metrics and adds output metric
 #' library(sp)
 #' library(ggplot2)
 #' library(plyr)
-#' shapefilepath<-mass_cd() #identify the path of the sample shapefile of congressional districts
-#' shape<-readOGR(shapefilepath) #create a spatial data frame
+#' shapefilepath     <- mass_cd()                    #identify the path of the sample shapefile of congressional districts
+#' shape             <- readOGR(shapefilepath)       #create a spatial data frame
 #' #Specify and equal area coordinate reference system
-#' proj=test<-CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m")
-#' reproj<-spTransform(shape,proj) #reproject the data to the specified CRS
-#' reproj@data["id"]<-rownames(shape@data) #Create an ID number for each polgyon
-#' shape2<-fortify(reproj) #fortify the data, converting it to a vector of XY values
-#' shpvector<-fortify(reproj) #create a vector of the nodes in the polygons for the spatial data frame
-#' shpperim<-PerimeterCalc(shpvector) #Calculate the perimeter of the polygons and return a list of values
-#' shpvector@data["Perimeter"]<-shpperim #Add a column of the perimeter values to the spatial data frame
+#' proj              <- test<-CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m")
+#' reproj            <- spTransform(shape,proj)      #reproject the data to the specified CRS
+#' reproj@data["id"] <- rownames(shape@data)         #Create an ID number for each polgyon
+#' shape2            <- fortify(reproj)              #fortify the data, converting it to a vector of XY values
+#' shpvector         <- fortify(reproj)              #create a vector of the nodes in the polygons for the spatial data frame
+#' shpperim          <- PerimeterCalc(shpvector)     #Calculate the perimeter of the polygons and return a list of values
+#' shpvector@data["Perimeter"]<-shpperim             #Add a column of the perimeter values to the spatial data frame
 #' ggplot()+
 #'   geom_polygon(data=shape3,aes(x=long,y=lat,group=group,fill=shpvector$Perimeter))     
 #'  
