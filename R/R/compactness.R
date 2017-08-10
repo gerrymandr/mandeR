002eab863f9ab7623d2da116d49de074e34fcd9c
johnpconnors <johnsbulkbox@gmail.com>
2017-08-10 15:52:33 -0400

    updated descriptions on functions

--- R/R/compactness.R
+++ R/R/compactness.R
@@ -88,36 +88,94 @@ PerimeterCalc<-function(lon,lat,id){
   gerry:::Perimeter(lon,lat,id)
 }
 
-#' @title Calculate the Polsby Popper metric for each polygon in a spatial data frame
+#' @title Calculate the Covnex Hull / Area metric for each polygon in a spatial 
+#'        data frame
 #'
 #' @description
-#'        Returns a vector of values with the Polsby Popper metric for all polygons in the Spatial Data Frame.
+#'        Returns a vector of values with the Convex Hull / Area metric for all 
+#'        polygons in the Spatial Data Frame.
 #'
-#' @param nodevector A vector of the XY coordinates for all nodes in a polygon - generated using sp.fortify()
-#'
-#' @return A vector of the perimeter measurements for each polygon in the Spatial Data Frame.
+#' @param id   A vector of identifiers for the fortified spatial data - allowing 
+#'             data to be joined to original spatial data frame
+#' @param lon  A vector of longitudes for all nodes created by fortifying the 
+#'             spatial data frame
+#' @param lat  A vector of latitudes for all nodes created by fortifying the 
+#'             spatial data frame
+#' 
+#' @return  A vector of the perimeter measurements for each polygon in the 
+#'          Spatial Data Frame.
 #'
 #' @examples
-#'
-#'    
+#'   
 #' library(sp)
 #' library(ggplot2)
 #' library(plyr)
-#' shapefilepath<-mass_cd() #identify the path of the sample shapefile of congressional districts
+#' #identify the path of the sample shapefile of congressional districts
+#' shapefilepath<-mass_cd() 
+#' shape<-readOGR(shapefilepath) #create a spatial data frame
+#' #Specify and equal area coordinate reference system
+#' proj=test<-CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 
+#'  +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m")
+#' reproj<-spTransform(shape,proj) #reproject the data to the specified CRS
+#' reproj@data["id"]<-rownames(shape@data) #Create an ID number for each polgyon
+#' shape2<-sp.fortify(reproj) #fortify the data, converting it to a vector of XY
+#' values
+#' #create a vector of the nodes in the polygons for the spatial data frame'
+#' shpvector<-sp.fortify(reproj) 
+#' #Calculate the Covex Hull metrics of the polygons and return a list of values
+#' hull <- gConvexHull(lon,lat,id) 
+#' shpvector@data["ConvexHull"]<-hull #Add a column of the perimeter values to 
+#' the spatial data frame
+#' ggplot()+
+#'   geom_polygon(data=shape3,aes(x=long,y=lat,group=group,
+#'   fill=shpvector$ConvexHull))     
+#' 
+#' @export
+
+PolsbyPopper<-function(nodevector){
+  PolsbyPopper(nodevector)
+}
+
+#' @title Calculate the Covnex Hull / Area metric for each polygon in a spatial data frame
+#'
+#' @description
+#'        Returns a vector of values with the Convex Hull / Area metric for all polygons in the Spatial Data Frame.
+#'
+#' @param id A vector of identifiers for the fortified spatial data - allowing data to be joined to original spatial data frame
+#' @param lon A vector of longitudes for all nodes created by fortifying the spatial data frame
+#' @param lat A vector of latitudes for all nodes created by fortifying the spatial data frame
+#' 
+#' @return A vector of the perimeter measurements for each polygon in the Spatial Data Frame.
+#'
+#' @examples
+#'   
+#' library(sp)
+#' library(ggplot2)
+#' library(plyr)
+#' #identify the path of the sample shapefile of congressional districts
+#' shapefilepath<-mass_cd() 
 #' shape<-readOGR(shapefilepath) #create a spatial data frame
 #' #Specify and equal area coordinate reference system
 #' proj=test<-CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m")
 #' reproj<-spTransform(shape,proj) #reproject the data to the specified CRS
 #' reproj@data["id"]<-rownames(shape@data) #Create an ID number for each polgyon
 #' shape2<-fortify(reproj) #fortify the data, converting it to a vector of XY values
-#' shpvector<-fortify(reproj) #create a vector of the nodes in the polygons for the spatial data frame
-#' polsbypop<-PolsbyPopper(shpvector) #Calculate the Polsby Popper metrics of the polygons and return a list of values
-#' shpvector@data["PolsbyPopper"]<-polsbypop #Add a column of the perimeter values to the spatial data frame
+#' #create a vector of the nodes in the polygons for the spatial data frame'
+#' shpvector<-fortify(reproj) 
+#' #Calculate the Covex Hull metrics of the polygons and return a list of values
+#' hull <- gConvexHull(lon,lat,id) 
+#' shpvector@data["ConvexHull"]<-hull #Add a column of the perimeter values to the spatial data frame
 #' ggplot()+
-#'   geom_polygon(data=shape3,aes(x=long,y=lat,group=group,fill=shpvector$PolsbyPopper))     
-#'  
+#'   geom_polygon(data=shape3,aes(x=long,y=lat,group=group,fill=shpvector$ConvexHull))     
+#' 
 #' @export
 
-PolsbyPopper<-function(nodevector){
-  PolsbyPopper(nodevector)
+ConvexHull<-function(x,y,id=NA){
+  if(is.na(id)){
+    ConvexHull(x,y)
+  }
+  else{
+    ConvexHull(x,y,id)
+  }
+
 }
